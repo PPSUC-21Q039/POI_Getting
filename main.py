f35408480f34a61b4c52627e9e2f9b8077faca01
Hu Wenqiang <huwenqiang.hwq@protonmail.com>
2022-02-16 21:49:55 +0800

    Added note: Six decimal places!

--- main.py
+++ main.py
@@ -36,3 +36,5 @@ if __name__ == "__main__":
             material_position_data = str(value[hexagon_id_key]) # value[hexagon_id_key] 为六边形顶点的坐标值的集合，共七个为一组
             processeed_position = material_position_data.replace('[[', '').replace(' ', '').replace('],[', '|').replace(']]', '')
             print(get_poi(processeed_position))
+
+    # print(get_poi('39.626978,116.081571|39.623717,116.084278|39.624504,116.089285|39.628551,116.091587|39.631813,116.088881|39.631026,116.083873|39.6269787,116.081571')) # 小数位数六位！！！！！！
