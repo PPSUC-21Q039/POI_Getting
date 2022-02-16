3d2bc08537700fe98a4cb14c4475e289f4b4e211
Hu Wenqiang <huwenqiang.hwq@protonmail.com>
2022-02-16 21:54:52 +0800

    Corrected the order

--- main.py
+++ main.py
@@ -29,12 +29,13 @@ if __name__ == "__main__":
     with open('./station_split_by_h3.json', 'r', encoding='utf8') as fp:
         json_data = json.load(fp)
         
-    for police_station_key, value in json_data.items(): # police_station_key 为该派出所的名称，vlaue 为其后的所有键值对
-        print(police_station_key) 
-        for hexagon_id_key in value:
-            print(hexagon_id_key) # hexagon_id_key 为每个六边形区域的ID
-            material_position_data = str(value[hexagon_id_key]) # value[hexagon_id_key] 为六边形顶点的坐标值的集合，共七个为一组
-            processeed_position = material_position_data.replace('[[', '').replace(' ', '').replace('],[', '|').replace(']]', '')
-            print(get_poi(processeed_position))
+    # for police_station_key, value in json_data.items(): # police_station_key 为该派出所的名称，vlaue 为其后的所有键值对
+    #     print(police_station_key) 
+    #     for hexagon_id_key in value:
+    #         print(hexagon_id_key) # hexagon_id_key 为每个六边形区域的ID
+    #         material_position_data = str(value[hexagon_id_key]) # value[hexagon_id_key] 为六边形顶点的坐标值的集合，共七个为一组
+    #         processeed_position = material_position_data.replace('[[', '').replace(' ', '').replace('],[', '|').replace(']]', '')
+    #         print(get_poi(processeed_position))
 
     # print(get_poi('39.626978,116.081571|39.623717,116.084278|39.624504,116.089285|39.628551,116.091587|39.631813,116.088881|39.631026,116.083873|39.6269787,116.081571')) # 小数位数六位！！！！！！
+    print(get_poi('116.081571,39.626978|116.084278,39.623717|116.089285,39.624504|116.091587,39.628551|116.088881,39.631813|116.083873,39.631026|116.081571,39.6269787')) # 小数位数六位！！！！！！ & 先经度后纬度！！！！
