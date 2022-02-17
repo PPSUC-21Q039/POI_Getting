c13e1f13daeb8568267991dc98180cb5fc5a238c
Manjaro Old PC <huwenqiang.hwq@protonmail.com>
2022-02-17 11:52:27 +0800

    Added Canter Position Process Module

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ import urllib
 import json
 from requests.api import request
 
+USER_KEY = 'aad49afa17b46e85e060bbe252f25a80' # 用户 Key
+
 # 暂时不用 try，不然没法 Ctrl + c 中止
 # def get_poi(processeed_position):
 #     url = 'https://restapi.amap.com/v5/place/polygon?' + 'key=aad49afa17b46e85e060bbe252f25a80' + '&polygon=' + str(processeed_position).strip()
@@ -27,7 +29,7 @@ from requests.api import request
 #         return '-1' # 网络错误等
 
 def get_poi(processeed_position, result_types):
-    url = 'https://restapi.amap.com/v5/place/polygon?' + 'key=aad49afa17b46e85e060bbe252f25a80' + '&polygon=' + str(processeed_position).strip() + '&types=' + str(result_types).strip()
+    url = 'https://restapi.amap.com/v5/place/polygon?' + 'key=' + USER_KEY.strip() + '&polygon=' + str(processeed_position).strip() + '&types=' + str(result_types).strip()
     response = urllib.request.urlopen(url)
     returned_data = json.load(response)
 
@@ -38,8 +40,22 @@ def get_poi(processeed_position, result_types):
     if (returned_data['status'] == '0'):
         return 'Error: 查询状态有误!' # 未获取到信息
     
-def get_center_position():
+def get_center_position(center_position_longtitude, center_position_latitude):
+
     return
+
+def get_location(returned_information_format, input_longtitude, input_latitude):
+    # Url example: https://restapi.amap.com/v3/geocode/regeo?output=xml&location=116.310003,39.991957&key=<用户的key>&radius=1000&extensions=all (all/base)
+    if (str(input_longtitude).strip() != 'NaN' and str(input_latitude).strip() != 'NaN'):
+        url = 'https://restapi.amap.com/v3/geocode/regeo?output=' + str(returned_information_format).strip() + 'xml&location=' + str(input_longtitude).strip() + ',' + str(input_latitude).strip() + '&key=' + USER_KEY.strip() + '&radius=1000' + '&extensions=base'
+        try:
+            response = urllib.request.urlopen(url)
+            returned_data = json.load(response)
+            return returned_data["regeocode"]["formatted_address"]
+        except:
+            return 'Error: 查询状态有误!'
+    else:
+        return 'Error: 输入有误!'
 
 if __name__ == "__main__":
     with open('./station_split_by_h3.json', 'r', encoding='utf8') as fp:
@@ -72,6 +88,7 @@ if __name__ == "__main__":
             center_position_latitude = center_position_latitude / 6
             # Example: 
             # ID: 8831818741fffff , 坐标: 116.0841505 , 39.671631166666664
-            print ('ID:', hexagon_id_key, ', 坐标:', center_position_longtitude, ',', center_position_latitude)
-
+            print ('ID:', hexagon_id_key, ', 坐标:', center_position_longtitude, ',', center_position_latitude, ', 地址:', get_location('json', center_position_longtitude, center_position_latitude))
+            
+            print ()
             print (get_poi(result_position, '130000|150000')) # Search Types 为：政府机构及社会团体 与 交通设施服务，编号分别为 150000 和 130000,用 '|' 分隔
