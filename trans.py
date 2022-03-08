391670e614aae3986de105b2ee219dedf96f4fd3
Huwenqiang Xiaomi Manjaro <49776858+Hu-Wenqiang@users.noreply.github.com>
2022-03-08 20:03:42 +0800

    Updated the directory

--- trans.py
+++
@@ -1,8 +0,0 @@
-import json
-INPUT_FILE = 'result_dict_list_final.json'
-# OUTPUT_FILE = 'result_dict_list_final.json' 
-with open(INPUT_FILE, 'r', encoding='utf8') as fp:
-    json_data = json.load(fp)
-    print(json_data)
-#with open(OUTPUT_FILE, 'w'):
-#    json_data.write(json_str)
